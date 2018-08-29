d9c02a028474e393b79e87cf6675a4a207de1cb6
Obiwac <obiwac@gmail.com>
2018-08-29 19:09:37 +0200

    Now will automatically install/run assembler extension if ROM.zed is not found

--- build.sh
+++ build.sh
@@ -7,6 +7,22 @@ if [ ! -d "asm/" ]; then
 	mv AQUA-CW asm
 fi
 
+if [ ! -f "ROM.zed" ]; then
+	echo "WARNING ROM file was not found (ROM.zed), so attempting to install an extension (or run it if it already exists) so you can write C programs and compile them into ROMs ..."
+	echo "NOTE    You will have to create assembler/c/code/main.c to compile, obviously (all your headers will go in the same folder). Press enter to continue ..."
+	read a
+	
+	if [ ! -d "assembler/" ]; then
+		git clone https://github.com/inobulles/AQUA-assembler
+		mv AQUA-assembler assembler
+	fi
+	
+	cd assembler/
+	sh compile_c.sh
+	cd ../
+	mv assembler/ROM.zed ROM.zed
+fi
+
 gcc glue.c -Werror -Wall -Wno-unused-variable -Wno-unused-but-set-variable -Wno-main -lSDL2 -lGL -lGLU -lSDL2_ttf
 ./a.out
 
