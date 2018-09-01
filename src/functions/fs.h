082b84e648590f7281d72375dfd8e4f3210475ba
Obiwac <obiwac@gmail.com>
2018-09-01 20:47:35 +0200

    Now fs_write does something

--- src/functions/fs.h
+++ src/functions/fs.h
@@ -5,12 +5,19 @@
 	#include "../macros_and_inclusions.h"
 	
 	unsigned long long fs_support(void) {
-		return 0;
+		return 1;
 		
 	}
 	
 	void fs_read(const char* path, char** data, unsigned long long* bytes) { KOS_TODO }
-	void fs_write(const char* path, const char* data, unsigned long long* bytes) { KOS_TODO }
 	void fs_free(char** data, unsigned long long bytes) { KOS_TODO }
+	
+	unsigned long long fs_write(const char* path, const char* data, unsigned long long bytes) {
+		FILE* file = fopen(path, "wb");
+		fwrite((const void*) data, sizeof(char), bytes, file);
+		fclose(file);
+		return 0;
+		
+	}
 	
 #endif
