8dc654663d976397994968994f28a42cfac83e7b
Obiwac <obiwac@gmail.com>
2018-09-12 20:11:49 +0200

    Fixed time not advancing

--- src/functions/system.h
+++ src/functions/system.h
@@ -125,12 +125,8 @@
 				break;
 				
 			} case DEVICE_CLOCK: {
-				if (!kos_tm_struct) {
-					printf("KOS initializing the tm struct ...\n");
-					kos_time = time(NULL);
-					kos_tm_struct = localtime(&kos_time);
-					
-				}
+				kos_time = time(NULL);
+				kos_tm_struct = localtime(&kos_time);
 				
 				if (strcmp(extra, "current") == 0) {
 					previous_time_device.hour     = (uint64_t) kos_tm_struct->tm_hour;
