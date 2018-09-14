97c870d9228e84da0e1120d0ed654629b3d1d32c
Obiwac <obiwac@gmail.com>
2018-09-14 07:43:45 +0200

    Added get_predefined_texture_width and get_predefined_texture_height

--- src/functions/video.h
+++ src/functions/video.h
@@ -117,6 +117,9 @@
 	static texture_t predefined_background_texture;
 	static texture_t predefined_frost_background_texture;
 	
+	static unsigned long long predefined_background_texture_dimensions      [2];
+	static unsigned long long predefined_frost_background_texture_dimensions[2];
+	
 	void bmp_load(bitmap_image_t* this, unsigned long long _path);
 	void bmp_free(bitmap_image_t* this);
 	
@@ -126,11 +129,19 @@
 		
 		bmp_load(&temp_bmp, (unsigned long long) "wallpaper/wallpaper.bmp");
 		predefined_background_texture = texture_create(temp_bmp.data, temp_bmp.bpp, temp_bmp.width, temp_bmp.height);
+		
+		predefined_background_texture_dimensions[0] = temp_bmp.width;
+		predefined_background_texture_dimensions[1] = temp_bmp.height;
+		
 		bmp_free(&temp_bmp);
 		warning += predefined_background_texture == -1;
 		
 		bmp_load(&temp_bmp, (unsigned long long) "wallpaper/frost.bmp");
 		predefined_frost_background_texture = texture_create(temp_bmp.data, temp_bmp.bpp, temp_bmp.width, temp_bmp.height);
+		
+		predefined_frost_background_texture_dimensions[0] = temp_bmp.width;
+		predefined_frost_background_texture_dimensions[1] = temp_bmp.height;
+		
 		bmp_free(&temp_bmp);
 		warning += predefined_frost_background_texture == -1;
 		
@@ -158,6 +169,24 @@
 		}
 		
 	}
+	
+	static unsigned long long* get_predefined_texture_size(unsigned long long name) {
+		switch (name) {
+			case TEXTURE_BACKGROUND:         return predefined_background_texture_dimensions;
+			case TEXTURE_FROSTED_BACKGROUND: return predefined_frost_background_texture_dimensions;
+			
+			default: {
+				printf("WARNING Texture %lld is unknown. Returning 0 ...\n", name);
+				return 0;
+				
+			}
+			
+		}
+		
+	}
+	
+	unsigned long long get_predefined_texture_width (unsigned long long name) { return get_predefined_texture_size(name)[0]; }
+	unsigned long long get_predefined_texture_height(unsigned long long name) { return get_predefined_texture_size(name)[1]; }
 	
 	void update_predefined_texture(unsigned long long name) {
 		switch (name) {
