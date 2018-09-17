883560fbeb9221665370ccd18e619f03d3a10cc4
Obiwac <obiwac@gmail.com>
2018-09-17 19:45:22 +0200

    Fixed create_texture_from_screenshot

--- src/functions/screenshot.h
+++ src/functions/screenshot.h
@@ -5,13 +5,27 @@
 	#define CREATE_TEXTURE_FROM_SCREENSHOT_FUNCTION 1
 	
 	texture_t create_texture_from_screenshot(void) {
-		unsigned long long bpp = 32;
+		unsigned long long bpp   = 32;
+		unsigned long long bytes = video_width() * video_height() * (bpp / 8);
 		
-		unsigned char* pixels = (unsigned char*) malloc(video_width() * video_height() * (bpp / 8));
+		unsigned char* pixels = (unsigned char*) malloc(bytes);
+		glReadBuffer(GL_FRONT);
 		glReadPixels(0, 0, video_width(), video_height(), GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 		
-		texture_t texture = texture_create((unsigned long long*) pixels, bpp, video_width(), video_height());
+		unsigned long long* data  = (unsigned long long*) malloc(bytes);
+		unsigned char*      data8 = (unsigned char*)      data;
+		unsigned long long  pitch = (unsigned long long)  video_width() * (bpp / 8);
+		
+		int y;
+		for (y = 0; y < video_height(); y++) {
+			memcpy(data8 + (video_height() - y - 1) * pitch, pixels + y * pitch, pitch);
+			
+		}
+		
+		texture_t texture = texture_create(data, bpp, video_width(), video_height());
+		
 		free(pixels);
+		free(data);
 		
 		if (texture < 0) {
 			printf("WARNING Could not create texture from screenshot\n");
