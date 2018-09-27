7cde9052f61e0866e83a2cf2538afdfedc11d692
Obiwac <obiwac@gmail.com>
2018-09-27 15:05:05 +0200

    Added fs_list function

--- src/functions/fs_list.h
+++ src/functions/fs_list.h
@@ -5,6 +5,8 @@
 	#include <dirent.h>
 	#define FS_LIST_FUNCTIONS 1
 	
+	#define FS_LIST_D_NAME_VALID (strcmp(directory->d_name, "..") != 0 && strcmp(directory->d_name, ".") != 0)
+	
 	unsigned long long fs_list_count(const char* path) {
 		unsigned long long count = 0;
 		
@@ -13,7 +15,7 @@
 		
 		if (dp) {
 			while ((directory = readdir(dp)) != NULL) {
-				count++;
+				count += FS_LIST_D_NAME_VALID;
 				
 			}
 			
@@ -29,8 +31,32 @@
 	}
 	
 	char** fs_list(const char* path) {
-		KOS_TODO;
-		return (char**) 0;
+		unsigned long long count   = fs_list_count(path);
+		unsigned long long current = 0;
+		char** result              = (char**) malloc(count * sizeof(char*));
+		
+		DIR* dp = opendir(path);
+		struct dirent* directory;
+		
+		if (dp) {
+			while ((directory = readdir(dp)) != NULL) {
+				if (FS_LIST_D_NAME_VALID) {
+					unsigned long long bytes = strlen(directory->d_name) + 1;
+					result[current] = (char*) malloc(bytes * sizeof(char));
+					memcpy(result[current++], directory->d_name, bytes);
+					
+				}
+				
+			}
+			
+			closedir(dp);
+			return result;
+			
+		} else {
+			printf("WARNING Directory `%s` could not be opened (for listing)\n", path);
+			return (char**) 0;
+			
+		}
 		
 	}
 	
