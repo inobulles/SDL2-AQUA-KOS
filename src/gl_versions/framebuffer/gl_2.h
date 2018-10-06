8a2a8f38711dd88fc4b6285f2669f1ff6971786f
Obiwac <obiwac@gmail.com>
2018-10-06 18:20:34 +0200

    Cleanup

--- src/gl_versions/framebuffer/gl_2.h
+++ src/gl_versions/framebuffer/gl_2.h
@@ -5,28 +5,26 @@
 	#include "../../macros_and_inclusions.h"
 	#include "../../gl_common/texture.h"
 	
-	int glDeleteFramebuffers();
-	int glCheckFramebufferStatus();
-	int glDrawBuffers();
-	int glFramebufferTexture();
+	int glDeleteFramebuffers     ();
+	int glCheckFramebufferStatus ();
+	int glDrawBuffers            ();
+	int glFramebufferTexture     ();
 	int glFramebufferRenderbuffer();
-	int glRenderbufferStorage();
-	int glBindRenderbuffer();
-	int glGenRenderbuffers();
-	int glBindFramebuffer();
-	int glGenFramebuffers();
+	int glRenderbufferStorage    ();
+	int glBindRenderbuffer       ();
+	int glGenRenderbuffers       ();
+	int glBindFramebuffer        ();
+	int glGenFramebuffers        ();
 	
 	framebuffer_t gl2_framebuffer_create(texture_t texture) {
-		//~ GLint                                  old_framebuffer;
-		//~ glGetIntegerv(GL_FRAMEBUFFER_BINDING, &old_framebuffer);
+		GLint                                  old_framebuffer;
+		glGetIntegerv(GL_FRAMEBUFFER_BINDING, &old_framebuffer);
 		
 		GLuint framebuffer_id = 0;
 		glGenFramebuffers(1, &framebuffer_id);
 		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_id);
-		glDrawBuffer(GL_COLOR_ATTACHMENT0);
 		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, texture, 0);
-		
-		//~ glBindFramebuffer   (GL_FRAMEBUFFER, old_framebuffer);
+		glBindFramebuffer   (GL_FRAMEBUFFER, old_framebuffer);
 		
 		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
 			printf("WARNING Failed to create framebuffer\n");
