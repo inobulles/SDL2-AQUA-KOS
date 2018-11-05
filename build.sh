71183b6984cb777479811bdb5cb26d37508014b7
Obiwac <obiwac@gmail.com>
2018-11-05 10:24:26 +0100

    Completely revamped build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,27 @@
 
-#!/bin/bash
+#!/bin/sh
 
-if [ "$2" = "" ]; then
+echo "INFO    Parsing arguments ..."
+
+no_note=""
+no_compile=""
+no_update=""
+remote=""
+execute=""
+
+while test $# -gt 0; do
+	if [ "$1" = "no-note"    ]; then no_note="true";    fi
+	if [ "$1" = "no-compile" ]; then no_compile="true"; fi
+	if [ "$1" = "no-update"  ]; then no_update="true";  fi
+	if [ "$1" = "remote"     ]; then remote="true";     fi
+	if [ "$1" = "execute"    ]; then execute="true";    fi
+	
+	shift
+done
+
+if [ no-compile = "" ]; then
+	echo "INFO    Testing for libraries ..."
+	
     has_curl_args=""
     has_discord_args=""
     
@@ -12,17 +32,23 @@ if [ "$2" = "" ]; then
     ld $discord_link && has_discord_args="-D__HAS_DISCORD $discord_link"
 fi
 
-if [ -d "assembler/c/.hidden/lib" ]; then
+if [ -d "assembler/c/.hidden/lib" ] && [ no-update = "" ]; then
+	echo "INFO    Updating the standard library ..."
+	
 	cd assembler/c/.hidden/lib
 	git pull origin master
 	cd ../../../../
 fi
 
 if [ ! -d "asm/" ]; then
+	echo "INFO    Installing the CW ..."
 	set -e
+	
 	git clone https://github.com/inobulles/AQUA-CW
 	mv AQUA-CW asm
-else
+elif [ no_update = "" ]; then
+	echo "INFO    Updating the CW ..."
+	
 	cd asm/
 	git pull origin master
 	cd ../
@@ -32,15 +58,25 @@ fi
 if [ ! -f "ROM.zed" ]; then
 	echo "WARNING ROM file was not found (ROM.zed), so attempting to install an extension (or run it if it already exists) so you can write C programs and compile them into ROMs ..."
 	
-	if [ "$1" = "" ]; then
+	if [ no_note = "" ]; then
 		echo "NOTE    You will have to create assembler/c/code/main.c to compile, obviously (all your headers will go in the same folder). Press enter to continue ..."
 		read a
 	fi
 	
 	if [ ! -d "assembler/" ]; then
+		echo "INFO    Installing the assembler ..."
+		
 		git clone https://github.com/inobulles/AQUA-assembler
 		mv AQUA-assembler assembler
+	elif [ no_update = "" ]; then
+		echo "INFO    Updating the assembler ..."
+		
+		cd assembler/
+		git pull origin master
+		cd ../
 	fi
+	
+	echo "INFO    Compiling code ..."
 	
 	cd assembler/
 	sh compile_c.sh
@@ -48,13 +84,21 @@ if [ ! -f "ROM.zed" ]; then
 	mv assembler/ROM.zed ROM.zed
 fi
 
-if [ "$2" = "" ]; then
+if [ ! -f "a.out" ] || [ no_compile = "" ]; then
+	echo "INFO    Compiling KOS ..."
+	
 	gcc glue.c -std=gnu99 -Wall \
 		-Wno-unused-variable -Wno-unused-but-set-variable -Wno-main \
 		-lSDL2 -lGL -lGLU -lSDL2_ttf -lm \
 		$has_curl_args $has_discord_args
 	
+	execute="true"
+fi
+
+if [ execute != "" ]; then
+	echo "INFO    Executing KOS ..."
 	./a.out
 fi
 
+echo "INFO    build.sh finished"
 exit 0
